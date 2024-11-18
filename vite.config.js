8314fec70b8bd10ff72a727b1ea13ef8352aee9f
makumbosiwale <siwalemalumbo@ymail.com>
2024-11-18 20:55:47 +0200

    finished update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  build: {
+    outDir: 'dist', // Default output directory
+    assetsDir: 'assets', // Default assets directory
+  },
+  base: './', // Use relative paths
   plugins: [react()],
 })
